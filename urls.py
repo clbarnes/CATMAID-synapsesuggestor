bfc792281dd5237fcafb9fd6d7b7c29303f13e8a
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-05-11 13:36:12 -0400

    boilerplate for urls

---
+++ urls.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+from synapsesuggestor.control import (skeleton_association as skel_assoc, synapse_detection as syn_det)
+
+
+urlpatterns = [
+    # url(pattern, method)
+]
