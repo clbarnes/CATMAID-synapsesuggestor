9863b438ec4338f49b19195d3dc99bb071798264
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-05-31 17:16:23 -0400

    admin: modeladmins for all models

--- synapsesuggestor/admin.py
+++ synapsesuggestor/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from synapsesuggestor.models import (
+    SynapseDetectionTiling, SynapseDetectionAlgorithm, SynapseImageStore,
+    SynapseSuggestionWorkflow, SynapseDetectionTile, SynapseSlice, SynapseObject,
+    SynapseSliceSynapseObject, SynapseAssociationAlgorithm,
+    ProjectSynapseSuggestionWorkflow, SynapseSliceTreenode
+)
+
+
+admin.site.register(SynapseDetectionTiling)
+
+admin.site.register(SynapseDetectionAlgorithm)
+
+admin.site.register(SynapseImageStore)
+
+admin.site.register(SynapseSuggestionWorkflow)
+
+admin.site.register(SynapseDetectionTile)
+
+class SynapseSliceAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(SynapseSlice, SynapseSliceAdmin)
+
+admin.site.register(SynapseObject)
+
+admin.site.register(SynapseSliceSynapseObject)
+
+admin.site.register(SynapseAssociationAlgorithm)
+
+admin.site.register(ProjectSynapseSuggestionWorkflow)
+
+admin.site.register(SynapseSliceTreenode)
