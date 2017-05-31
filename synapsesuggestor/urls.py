87fc97a88b578d8968c499ac3be9dd968bc0045a
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-05-31 17:17:26 -0400

    urls: endpoints for all methods

--- synapsesuggestor/urls.py
+++ synapsesuggestor/urls.py
@@ -5,22 +5,23 @@ from django.conf.urls import url
 
 from synapsesuggestor.control import (treenode_association as node_assoc, synapse_detection as syn_det, common)
 
-# common
+app_name = 'synapsesuggestor'
 
-urlpatterns = [
-    url('^algo-version$', common.get_or_create_algo_version)
-]
+urlpatterns = []
 
 # synapse detection endpoints
 
 urlpatterns += [
-    url('^synapsedetection/', syn_det.)
+    url(r'^synapse-detection/tiles/detected$', syn_det.get_detected_tiles),
+    url(r'^synapse-detection/tiles/insert-synapse-slices$', syn_det.add_synapse_slices_from_tile),
+    url(r'^synapse-detection/slices/agglomerate$', syn_det.agglomerate_synapse_slices),
 ]
 
 # treenode association endpoints
 
 urlpatterns += [
-    url('^treenodeassociation/', node_assoc.)
+    url(r'^treenode-association/(?P<project_id>\d+)/get$', node_assoc.get_treenode_associations),
+    url(r'^treenode-association/(?P<project_id>\d+)/add$', node_assoc.add_treenode_synapse_associations),
 ]
 
 # analysis endpoints
