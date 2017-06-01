f7ed8c127c7f0786c8a01363fc708512ee62fd0d
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-01 14:34:19 -0400

    test syndet: basic test

---
+++ synapsesuggestor/tests/apis/test_synapse_detection.py
@@ -0,0 +1,6 @@
+from synapsesuggestor.tests.apis.common import SynapseSuggestorApiTestCase
+
+
+class SynapseDetectionApiTests(SynapseSuggestorApiTestCase):
+    def test_tests(self):
+        self.assertEquals(1, 1)
