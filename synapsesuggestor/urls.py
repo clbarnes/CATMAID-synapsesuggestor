a384d9a988082f1054d34ff603da540888ebca55
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-06 16:39:52 -0400

    urls: workflow endpoints

--- synapsesuggestor/urls.py
+++ synapsesuggestor/urls.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.conf.urls import url
 
-from synapsesuggestor.control import (treenode_association as node_assoc, synapse_detection as syn_det, common)
+from synapsesuggestor.control import (treenode_association as node_assoc, synapse_detection as syn_det, workflow)
 
 app_name = 'synapsesuggestor'
 
@@ -15,6 +15,7 @@ urlpatterns += [
     url(r'^synapse-detection/tiles/detected$', syn_det.get_detected_tiles),
     url(r'^synapse-detection/tiles/insert-synapse-slices$', syn_det.add_synapse_slices_from_tile),
     url(r'^synapse-detection/slices/agglomerate$', syn_det.agglomerate_synapse_slices),
+    url(r'^synapse-detection/workflow$', workflow.get_workflow),
 ]
 
 # treenode association endpoints
@@ -22,7 +23,12 @@ urlpatterns += [
 urlpatterns += [
     url(r'^treenode-association/(?P<project_id>\d+)/get$', node_assoc.get_treenode_associations),
     url(r'^treenode-association/(?P<project_id>\d+)/add$', node_assoc.add_treenode_synapse_associations),
+    url(r'^treenode-association/(?P<project_id>\d+)/add$', node_assoc.add_treenode_synapse_associations),
+    url(r'^treenode-association/(?P<project_id>\d+)/workflow', workflow.get_project_workflow),
 ]
+
+# workflow endpoints
+
 
 # analysis endpoints
 
