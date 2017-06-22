71736889ad6a4be7dc9985bd6fb82b517c3f9641
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-06-22 13:46:25 -0400

    node assoc api: docstrings

--- synapsesuggestor/control/treenode_association.py
+++ synapsesuggestor/control/treenode_association.py
@@ -16,21 +16,29 @@ from synapsesuggestor.control.common import list_into_query, get_most_recent_pro
 
 logger = logging.getLogger(__name__)
 
-
+@api_view(['POST'])
 def add_treenode_synapse_associations(request, project_id=None):
     """
     POST request which adds a set of treenode-synapse associations to the database.
-
-    Parameters
-    ----------
-    request
-    project_id
-
-    Returns
-    -------
-
+    ---
+    parameters:
+      - name: project_workflow_id
+        type: integer
+        required: false
+        description: ID of project synapse suggestion workflow
+      - name: associations
+        type: array
+        items:
+          type: array
+          items:
+            type: integer
+        required: false
+        description:> Array of JSON-encoded arrays of length 3, whose items are a synapse slice ID, treenode ID and
+          contact area estimation
     """
     # todo: add null -> tn.id association if no synapses are found
+    # todo: should take SSW and figure out PSSW itself
+    # todo: add return type
 
     pssw_id = int(request.POST.get(
         'project_workflow_id', get_most_recent_project_SS_workflow(project_id).id
@@ -42,7 +50,6 @@ def add_treenode_synapse_associations(request, project_id=None):
 
     rows = [(syn, treenode, contact_px, pssw_id) for syn, treenode, contact_px in associations]
 
-    # todo: add null association for unassociated treenodes
     query, cursor_args = list_into_query('''
         INSERT INTO synapse_slice_treenode (
           synapse_slice_id, treenode_id, contact_px, project_synapse_suggestion_workflow_id
@@ -57,22 +64,24 @@ def add_treenode_synapse_associations(request, project_id=None):
     return JsonResponse(cursor.fetchall(), safe=False)
 
 
+@api_view(['GET'])
 def get_treenode_associations(request, project_id=None):
     """
     GET request which takes a skeleton ID and returns its treenode associations with synapse objects under the synapse
     suggestion workflow most recently associated with this project.
-
-    skid
-
-    Parameters
-    ----------
-    request
-    project_id
-
-    Returns
-    -------
-
+    ---
+    parameters:
+      - name: skid
+        type: integer
+        required: true
+        description: Skeleton ID for which to get synapses
+      - name: project_workflow_id
+        type: integer
+        required: false
     """
+    # todo: take set of nodes instead
+    # todo: improve error handling
+
     skel_id = int(request.GET['skid'])
     pssw_id = int(request.GET.get(
         'project_workflow_id', get_most_recent_project_SS_workflow(project_id).id
