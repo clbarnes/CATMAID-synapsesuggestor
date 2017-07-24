ccfb22682d176fbf2d698d19b4a4a4eff50daf1d
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-07-24 14:25:57 -0400

    fix association/assocation typpo

--- synapsesuggestor/control/workflow.py
+++ synapsesuggestor/control/workflow.py
@@ -72,7 +72,7 @@ def _get_valid_workflows(project_id, stack_id):
     columns = [
         'workflow_id', 'detection_algo_id', 'detection_algo_hash', 'detection_algo_date',
         'detection_algo_notes',
-        'project_workflow_id', 'association_algo_id', 'assocation_algo_hash', 'association_algo_date',
+        'project_workflow_id', 'association_algo_id', 'association_algo_hash', 'association_algo_date',
         'association_algo_notes',
         'tile_height_px', 'tile_width_px'
     ]
