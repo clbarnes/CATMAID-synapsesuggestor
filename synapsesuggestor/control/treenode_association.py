7b0194198c36ca95fbae858668e9eb2abf50b43a
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2017-08-24 17:31:12 -0400

    function for getting synapses near skeleton

--- synapsesuggestor/control/treenode_association.py
+++ synapsesuggestor/control/treenode_association.py
@@ -11,7 +11,9 @@ from rest_framework.decorators import api_view
 
 from catmaid.control.authentication import requires_user_role
 from catmaid.control.common import get_request_list
-from synapsesuggestor.control.common import list_into_query, get_most_recent_project_SS_workflow
+from synapsesuggestor.models import ProjectSynapseSuggestionWorkflow
+from synapsesuggestor.control.common import list_into_query, get_most_recent_project_SS_workflow, \
+    get_project_SS_workflow, get_translation_resolution
 
 
 logger = logging.getLogger(__name__)
@@ -89,7 +91,6 @@ def get_treenode_associations(request, project_id=None):
 
     cursor = connection.cursor()
 
-    # todo: assert that slices are associated with the correct workflow
     cursor.execute('''
         SELECT sstn.treenode_id, ssso.synapse_object_id, sum(sstn.contact_px) FROM synapse_slice_treenode sstn
           INNER JOIN project_synapse_suggestion_workflow pssw
@@ -104,3 +105,91 @@ def get_treenode_associations(request, project_id=None):
     ''', (skel_id, pssw_id))
 
     return JsonResponse(cursor.fetchall(), safe=False)
+
+
+@api_view(['GET'])
+def get_synapse_slices_near_skeletons(request, project_id=None):
+    """
+    Find synapse slices which are within a given distance of a particular skeleton.
+
+    Returns {columns: [...], data: [[...], ...]}
+
+    synapse bounds are in stack coordinates
+
+    XY bounds are given in [xmin, ymin, xmax, ymax] order
+    ---
+    parameters:
+      - name: skid
+        type: integer
+        required: true
+        description: Skeleton ID for which to get synapses
+      - name: project_workflow_id
+        type: integer
+        required: false
+      - name: distance
+        type: float
+        required: false
+        description: distance, in nm, within which to find synapses
+    """
+    skel_id = int(request.GET['skid'])
+    pssw_id = int(request.GET.get('workflow_id', get_most_recent_project_SS_workflow(project_id).id))
+    distance = float(request.GET.get('distance', 0))
+    dimensions = 2  # int(request.GET.get('dimensions', 2))
+
+    cursor = connection.cursor()
+
+    ssw_id = ProjectSynapseSuggestionWorkflow.objects.get(id=pssw_id).synapse_suggestion_workflow_id
+
+    translation, resolution = get_translation_resolution(project_id, ssw_id, cursor)
+    offset_xs, offset_ys, offset_zs = translation / resolution
+
+    columns = ['skeleton_id', 'treenode_id', 'synapse_object_id', 'synapse_slice_ids', 'synapse_z_s',
+               'synapse_bounds_s']
+
+    if dimensions == 2:
+
+        cursor.execute('''
+            SELECT tn.skeleton_id, tn.id, ss_so2.synapse_object_id, array_agg(DISTINCT ss2.id), 
+              tile2.z_tile_idx, ARRAY[
+                ST_XMin(ST_Extent(ss2.convex_hull_2d)),
+                ST_YMin(ST_Extent(ss2.convex_hull_2d)),
+                ST_XMax(ST_Extent(ss2.convex_hull_2d)),
+                ST_YMax(ST_Extent(ss2.convex_hull_2d))
+              ]
+            FROM synapse_slice ss1
+            INNER JOIN synapse_detection_tile tile1
+              ON ss1.synapse_detection_tile_id = tile1.id
+            INNER JOIN treenode tn
+              ON tile1.z_tile_idx = (tn.location_z / %s) - %s
+              AND ST_DWithin(
+                    ST_MakePoint((tn.location_x / %s) - %s, (tn.location_y / %s) - %s),
+                    ss1.convex_hull_2d, 
+                    (%s / %s) - %s
+                )
+            INNER JOIN synapse_slice_synapse_object ss_so1
+              ON ss_so1.synapse_slice_id = ss1.id
+            INNER JOIN synapse_slice_synapse_object ss_so2
+              ON ss_so1.synapse_object_id = ss_so2.synapse_object_id
+            INNER JOIN synapse_slice ss2
+              ON ss_so2.synapse_slice_id = ss2.id
+            INNER JOIN synapse_detection_tile tile2
+              ON ss2.synapse_detection_tile_id = tile2.id
+            WHERE tn.skeleton_id = %s
+              AND tn.project_id = %s
+              AND tile1.synapse_suggestion_workflow_id = %s
+            GROUP BY ss_so2.synapse_object_id, tn.skeleton_id, tn.id, tile2.z_tile_idx;
+        ''', (
+                resolution[2], offset_zs,
+                resolution[0], offset_xs, resolution[1], offset_ys,
+                distance, resolution[0], offset_xs,  # assumes xy isotropy
+                skel_id, project_id, ssw_id
+            )
+        )
+
+        data = cursor.fetchall()
+    elif dimensions == 3:
+        raise NotImplementedError('Synapse slices near skeletons is only implemented for 2D search')
+    else:
+        raise ValueError('`dimensions` must be 2 or 3')
+
+    return JsonResponse({'columns': columns, 'data': data})
